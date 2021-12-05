8c0a3e712ddd61430f491c3ed7e25123d7d7d407
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-05 11:54:31 +0530

    Create Contact in Address Book

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Create Contact in Address Book
+ */
+
+using AddressBook;
+
+AddressBookEntry addressBook = new AddressBookEntry();
+Contact newContact =addressBook.CreateContact();
+
+Console.WriteLine("\n*****-> Details Of Person  <-*****");
+Console.WriteLine($"First Name: {newContact.FirstName}");
+Console.WriteLine($"Last Name: {newContact.LastName}");
+Console.WriteLine($"City Name: {newContact.City}");
+Console.WriteLine($"State Name: {newContact.State}");
+Console.WriteLine($"Zipcode: {newContact.ZipCode}");
+Console.WriteLine($"PhoneNumber: {newContact.PhoneNumber}");
+Console.WriteLine($"Email: {newContact.Email}");
+
