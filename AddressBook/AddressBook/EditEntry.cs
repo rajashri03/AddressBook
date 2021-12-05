417b137c25fba2b9edc6d265ecaea6c9eb223c75
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-05 20:05:27 +0530

    Edit Contact From Address Book

--- AddressBook/AddressBook/EditEntry.cs
+++ AddressBook/AddressBook/EditEntry.cs
@@ -24,7 +24,7 @@ namespace AddressBook
 
             Console.Write("Enter Last Name: ");
             person.Lastname = Console.ReadLine();
-            
+
             Console.Write("Enter City name: ");
             person.City = Console.ReadLine();
 
@@ -54,7 +54,7 @@ namespace AddressBook
             Console.WriteLine("Zip Code: " + person.Zipcode);
             Console.WriteLine("Phone Number: " + person.PhoneNumber);
             Console.WriteLine("Email ID: " + person.email);
-            
+
         }
         public static void ListAllContacts()
         {
@@ -65,7 +65,7 @@ namespace AddressBook
             {
                 AllDetails(person);
             }
-           
+
         }
 
         public static void Update()
@@ -86,70 +86,70 @@ namespace AddressBook
                         Console.ResetColor();
                         EditEntry.Update();
                     }
-                        Console.WriteLine("1)If u want to edit the Firstname");
-                        Console.WriteLine("2)If u want to edit the LastName");
-                       Console.WriteLine("3)If u want to edit the City");
-                        Console.WriteLine("4)If u want to edit the State");
-                        Console.WriteLine("5)If u want to edit the Zip Code");
-                        Console.WriteLine("6)If u want to edit the Phone Number");
-                        Console.WriteLine("7)If u want to edit the Email Id");
-                        int choice = Convert.ToInt32(Console.ReadLine());
-                        switch (choice)
-                        {
-                            case 1:
-                                Console.WriteLine("Enter New Firstname; ");
-                                person.Firstname = Console.ReadLine();
-                                People.Add(person);
-                                Console.WriteLine("First Name: " + person.Firstname);
-                                break;
-                            case 2:
-                                Console.WriteLine("Enter New LastName; ");
-                                person.Lastname = Console.ReadLine();
-                                People.Add(person);
-                                Console.WriteLine("Last Name: " + person.Lastname);
-                                break;
-                          
-                            case 3:
+                    Console.WriteLine("1)If u want to edit the Firstname");
+                    Console.WriteLine("2)If u want to edit the LastName");
+                    Console.WriteLine("3)If u want to edit the City");
+                    Console.WriteLine("4)If u want to edit the State");
+                    Console.WriteLine("5)If u want to edit the Zip Code");
+                    Console.WriteLine("6)If u want to edit the Phone Number");
+                    Console.WriteLine("7)If u want to edit the Email Id");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter New Firstname; ");
+                            person.Firstname = Console.ReadLine();
+                            People.Add(person);
+                            Console.WriteLine("First Name: " + person.Firstname);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter New LastName; ");
+                            person.Lastname = Console.ReadLine();
+                            People.Add(person);
+                            Console.WriteLine("Last Name: " + person.Lastname);
+                            break;
 
-                                Console.WriteLine("Enter New City: ");
-                                person.City = Console.ReadLine();
-                                People.Add(person);
-                                Console.WriteLine("City: " + person.City);
-                                break;
-                            case 4:
+                        case 3:
 
-                                Console.WriteLine("Enter New State: ");
-                                person.State = Console.ReadLine();
-                                People.Add(person);
-                                Console.WriteLine("State Name: " + person.State);
-                                break;
-                            case 5:
+                            Console.WriteLine("Enter New City: ");
+                            person.City = Console.ReadLine();
+                            People.Add(person);
+                            Console.WriteLine("City: " + person.City);
+                            break;
+                        case 4:
 
-                                Console.WriteLine("Enter New ZipCode: ");
-                                person.Zipcode = Console.ReadLine();
-                                People.Add(person);
-                                Console.WriteLine("ZipCode: " + person.Zipcode);
-                                break;
-                            case 6:
+                            Console.WriteLine("Enter New State: ");
+                            person.State = Console.ReadLine();
+                            People.Add(person);
+                            Console.WriteLine("State Name: " + person.State);
+                            break;
+                        case 5:
 
-                                Console.WriteLine("Enter New PhoneNumber: ");
-                                person.PhoneNumber = Console.ReadLine();
-                                People.Add(person);
-                                Console.WriteLine("PhoneNumber: " + person.PhoneNumber);
-                                break;
-                            case 7:
-                                Console.WriteLine("Enter New Email ID: ");
-                                person.email = Console.ReadLine();
-                                People.Add(person);
-                                Console.WriteLine("Email Id: " + person.email);
-                                break;
+                            Console.WriteLine("Enter New ZipCode: ");
+                            person.Zipcode = Console.ReadLine();
+                            People.Add(person);
+                            Console.WriteLine("ZipCode: " + person.Zipcode);
+                            break;
+                        case 6:
 
-                            default:
-                                Console.WriteLine("Invalid Choice");
-                                break;
-                        
+                            Console.WriteLine("Enter New PhoneNumber: ");
+                            person.PhoneNumber = Console.ReadLine();
+                            People.Add(person);
+                            Console.WriteLine("PhoneNumber: " + person.PhoneNumber);
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter New Email ID: ");
+                            person.email = Console.ReadLine();
+                            People.Add(person);
+                            Console.WriteLine("Email Id: " + person.email);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+
                     }
-                   ListAllContacts();
+                    ListAllContacts();
                     break;
                 default:
                     Console.WriteLine("Thanku!!!");
